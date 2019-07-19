d168a2c3ee7df7a19f7fb34281c5411ec502e13c
Anders Windelhed <anders.windelhed@gmail.com>
2019-07-19 17:56:28 +0200

    Adding missing files

---
+++ utils/valueArray.js
@@ -0,0 +1,7 @@
+const valueArray = (data, headers) => {
+  const values = headers.map(h => data[h])
+
+  return values
+}
+
+module.exports = valueArray
---
+++ utils/valueArray.test.js
@@ -0,0 +1,17 @@
+const valueArray = require('./valueArray')
+
+describe('valueArray', () => {
+  it('returns array with values at expected positions', () => {
+    const data = {
+      date: '2000-01-01',
+      val1: '1',
+      val2: 2,
+    }
+    const headers = ['date', 'val2', 'val1']
+    const expected = ['2000-01-01', 2, '1']
+
+    const result = valueArray(data, headers)
+
+    expect(result).toEqual(expected)
+  })
+})
